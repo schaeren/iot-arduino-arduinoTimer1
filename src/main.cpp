c86e1555e820d6dd86537b114b44f5b4506fc1a1
Peter Schären <peter.schaeren@swissteach.ch>
2022-03-01 16:07:42 +0100

    Initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include "Arduino.h"
+#include "arduino-timer.h"
+
+const int greenLedPin = 10; // green LED
+const int redLedPin = 12;   // red LED
+const int buttonPin = 2;    // button
+
+// Delay to be used to debounce button [ms]
+unsigned long debouncingTime = 100;
+// timer may be used for up to 2 concurrent timers, i.e. one interval timer
+// for green LED and one single-shot timer for red LED.
+// See also https://github.com/contrem/arduino-timer
+Timer<2> timer;
+
+unsigned long lastButtonDownTime = 0;
+
+bool onSwitchOffRedLed(void *);
+
+void onButtonDown() {
+    int oldButtonState = digitalRead(buttonPin);
+    unsigned int now = millis();
+    if (now - lastButtonDownTime > debouncingTime) {
+        Serial.print("Button DOWN, last call to onButtonDown() at ");
+        Serial.print(lastButtonDownTime);
+        Serial.print("ms - current time is ");
+        Serial.print(now);
+        Serial.println("ms");
+
+        lastButtonDownTime = now;
+        digitalWrite(redLedPin, HIGH);
+        timer.every(2000, onSwitchOffRedLed);
+    }
+}
+
+bool onSwitchOffRedLed(void *) {
+    digitalWrite(redLedPin, LOW);
+    return false; // stop timer
+}
+
+bool onTimer(void *) {
+    digitalWrite(greenLedPin, !digitalRead(greenLedPin));
+    return true; // keep timer running
+}
+
+void setup() { 
+    pinMode(greenLedPin, OUTPUT);
+    pinMode(redLedPin, OUTPUT);
+    pinMode(buttonPin, INPUT_PULLUP);
+    Serial.begin(115200);
+    attachInterrupt(digitalPinToInterrupt(buttonPin), onButtonDown, FALLING);
+    timer.every(100, onTimer); // timer for green led (blicking with 5Hz)
+}
+
+void loop() {
+    timer.tick();
+}
